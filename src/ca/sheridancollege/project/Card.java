e527a002167978c66cad8482550d61f9d82bba1c
Michael <DeqKnight@gmail.com>
2024-04-18 20:15:46 -0400

    finished card class

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -5,21 +5,29 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
  * @author dancye
  */
-public abstract class Card {
-    //default modifier for child classes
+public class Card {
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    @Override
-    public abstract String toString();
+
+    private static List<Integer> CARD_VALUES = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    private static List<String> CARD_SUITS = Arrays.asList("Club", "Diamonds", "Hearts", "Spades");
+    
+  
+
+    public static List<Integer> getCardValues() {
+        return CARD_VALUES;
+    }
+
+    public static List<String> getCardSuits() {
+        return CARD_SUITS;
+    }
 
 }
