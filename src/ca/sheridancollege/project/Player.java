dbcf93cdd462756880d6aec5e629e117c641113a
Michael <DeqKnight@gmail.com>
2024-04-18 20:17:56 -0400

    finished player class

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -5,23 +5,32 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  */
-public abstract class Player {
-
+public abstract class Player{
+    private List<String> handSuit;
+    private List<Integer> handValue;
     private String name; //the unique name for this player
-
+    private int total;
     /**
      * A constructor that allows you to set the player's unique ID
      *
      * @param name the unique ID to assign to this player.
+     * @param suit
+     * @param value
      */
-    public Player(String name) {
+    public Player(String name){
         this.name = name;
+        handSuit = new ArrayList<String>();
+        handValue = new ArrayList<Integer>();
+        
     }
 
     /**
@@ -44,6 +53,33 @@ public abstract class Player {
      * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
      * with logic to play your game.
      */
-    public abstract void play();
+    
+    
+    public void playTurn() {
+        createHand();
+        createHand();
+               
+        this.total = handValue.get(0) + handValue.get(1);
+        
+        System.out.println(getName() + "'s turn. \nHand: \n" + handSuit + "\n" + handValue + "\ntotal: " + total + "\n");
+        
+    }
 
+    private void createHand(){
+        Random rand = new Random();
+        int randValue = rand.nextInt(10);
+        int randSuit = rand.nextInt(4);
+        Deck deck = new Deck();
+        Card card = new Card();
+        
+        deck.setCardValue(randValue);
+        handValue.add(deck.getCardValue());
+        deck.setCardSuit(randSuit);
+        handSuit.add(deck.getCardSuit());
+    }
+                
+    public int getTotal(){
+        return total;
+    }
+       
 }
